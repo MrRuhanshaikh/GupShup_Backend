b4fc8c154673b81d52000e2c3eb4626b582355c2
Ruhan Shaikh <ruhan1192002@gmail.com>
2025-04-25 20:25:46 +0530

    Fixed path module import

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
@@ -12,7 +13,7 @@ import { app, server } from "./lib/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
-const __dirname = path.resolve();
+const __dirname = path.resolve(); 
 
 app.use(express.json());
 app.use(cookieParser());
